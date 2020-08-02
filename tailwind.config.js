e057423d2f296778c4de68041a386639a7c23847
Brad <brad@viziworx.com>
2020-08-02 14:42:46 -0400

    adds dependencies and configurations

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [require('@tailwindcss/ui')],
+};
