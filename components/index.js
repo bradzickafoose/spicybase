e71f89571dcd9de689382c877a743ff369072a45
Brad <brad@viziworx.com>
2020-08-18 22:57:45 -0400

    📦 NEW: adds a Link component with active class

--- components/index.js
+++ components/index.js
@@ -1,6 +1,7 @@
 export { Header } from './Header';
 export { Footer } from './Footer';
+export { Link } from './Link';
 export { Breadcrumbs } from './Breadcrumbs';
 export { PageTitle } from './PageTitle';
-export { JobForm, JobLink, JobDetail } from './Job';
+export { JobForm, JobLink } from './Job';
 export { ChatRooms } from './ChatRooms';
