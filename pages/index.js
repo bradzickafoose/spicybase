5dd72264adc3b7dc13060cb209fbb0f125a08920
Brad <brad@viziworx.com>
2020-08-18 23:08:24 -0400

    👌  IMPROVE: updates page title and styles import

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from 'styles/Home.module.css'
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<title>Create Next App</title>
+				<title>Spicybase</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
