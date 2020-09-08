f3686d4105f60985be2a8e24672ec5e6328c9a3b
Brad <brad@viziworx.com>
2020-09-07 22:24:07 -0400

    📦 NEW: adds Placeholder component

---
+++ components/common/placeholder-section/PlaceholderSection.jsx
@@ -0,0 +1,7 @@
+export default function EmptyPlaceholder() {
+  return (
+    <section className="px-4 py-8 sm:px-0">
+      <div className="border-4 border-gray-300 border-dashed rounded-lg h-96"></div>
+    </section>
+  );
+}
---
+++ components/common/placeholder-section/index.js
@@ -0,0 +1,1 @@
+export { default as PlaceholderSection } from './PlaceholderSection';
