497edbc2a754ed0ca2f726720162d0cbb41e58aa
Brad <brad@viziworx.com>
2020-09-07 22:24:30 -0400

    📦 NEW: adds Layout component

---
+++ components/layout/Layout.jsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import { Header, Footer, PageHeading } from 'components';
+
+export default function Layout({
+  title,
+  children,
+  header = true,
+  pageTitle = true,
+  footer = true,
+}) {
+  return (
+    <>
+      <Head>
+        <title>{title && `${title} - `}Spicybase</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div style={{ backgroundColor: '#F3EDE8' }}>
+        {header && <Header />}
+        <main>
+          {pageTitle && <PageHeading title={title} />}
+          {children}
+        </main>
+        <div id="__modals" />
+        {footer && <Footer />}
+      </div>
+    </>
+  );
+}
---
+++ components/layout/index.js
@@ -0,0 +1,4 @@
+export { Container } from './container';
+export { Footer } from './footer';
+export { Header } from './header';
+export { default as Layout } from './Layout';
