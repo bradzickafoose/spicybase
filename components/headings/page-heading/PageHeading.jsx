41cd0edfd50dadabd0430a5283cf72fab063a715
Brad <brad@viziworx.com>
2020-09-12 18:38:16 -0400

    👌 IMPROVE: updates heading width

--- components/headings/page-heading/PageHeading.jsx
+++ components/headings/page-heading/PageHeading.jsx
@@ -1,6 +1,6 @@
 export default function PageHeading({ title, children }) {
   return (
-    <div className="px-4 pt-10 mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div className="max-w-6xl px-4 pt-10 mx-auto sm:px-6 lg:px-8">
       {children}
       <h1 className="text-3xl font-bold leading-tight text-gray-700">
         {title}
