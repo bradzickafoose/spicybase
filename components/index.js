a1179360cd2b7922698a697e66604eb19d86ee88
Brad <brad@viziworx.com>
2020-09-12 18:34:00 -0400

    👌 IMPROVE: updates login form to use react hook form, adds form validation, and styling

--- components/index.js
+++ components/index.js
@@ -9,7 +9,7 @@ export {
   Modal,
   PlaceholderSection,
 } from './common';
-export { CreateJobForm, LoginForm } from './forms';
+export { CreateJobForm, LoginForm, RegistrationForm } from './forms';
 export { PageHeading } from './headings';
 export { Container, Footer, Header, Layout } from './layout';
 export { ChatRooms, JobLink } from './views';
--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,29 @@
+import Link from 'next/link';
 import { Layout, LoginForm } from 'components';
 
 export default function LoginPage() {
   return (
     <Layout header={false} footer={false} pageTitle={false}>
-      <LoginForm />
+      <div className="flex items-center justify-center min-h-screen px-4 py-12 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md">
+          <div>
+            <h2 className="mt-6 text-3xl font-extrabold leading-9 text-center text-cool-gray-700">
+              Log in to your account
+            </h2>
+          </div>
+          <div className="p-6 mt-6 bg-white rounded-lg shadow-xl">
+            <LoginForm />
+          </div>
+          <p className="mt-6 text-sm leading-5 text-center text-gray-600">
+            Or
+            <Link href="/register">
+              <a className="pl-1 font-medium text-blue-600 transition duration-150 ease-in-out hover:text-blue-500 focus:outline-none focus:underline">
+                create new account
+              </a>
+            </Link>
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 }
