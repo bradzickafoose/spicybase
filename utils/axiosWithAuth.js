0f76261eea1a4373a862c0f282a8bd435134e8e4
Brad <brad@viziworx.com>
2020-09-12 18:32:02 -0400

    🐛  FIX: updates localhost

--- utils/axiosWithAuth.js
+++ utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
 
   return axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'http://localhost:5000/',
     headers: {
       Authorization: token,
     },
