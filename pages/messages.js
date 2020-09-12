761b9fbd068d477ff9557015a1b766481155ced8
Brad <brad@viziworx.com>
2020-09-12 18:40:00 -0400

    👌 IMPROVE: removes footer from layout

--- pages/messages.js
+++ pages/messages.js
@@ -3,8 +3,8 @@ import requireUser from 'utils/requireUser';
 
 export default function MessagesPage() {
   return (
-    <Layout title="Messages">
-      <div className="flex flex-row flex-grow min-h-0 px-4 mx-auto sm:px-0 max-w-7xl sm:px-6 lg:px-8">
+    <Layout title="Messages" footer={false}>
+      <div className="flex flex-row flex-grow min-h-0 px-4 mx-auto bg-white sm:px-0 max-w-7xl sm:px-6 lg:px-8">
         <ChatRooms />
       </div>
     </Layout>
