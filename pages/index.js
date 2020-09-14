224f1bdcba848511444a428cc7e5be834e507bf1
Brad <brad@viziworx.com>
2020-09-13 23:44:26 -0400

    👌 IMPROVE: adds router to push users to profiles page

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,12 @@
-import Link from 'next/link';
-import { Layout } from 'components';
-import { APP_NAME, BASE_URL } from 'utils/constants';
-import styles from 'styles/Home.module.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function HomePage() {
-  return (
-    <Layout pageTitle={false}>
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Welcome to <a href={BASE_URL}>{APP_NAME}</a>
-          </h1>
+  const router = useRouter();
 
-          <p className={styles.description}>
-            <Link href="/login">
-              <a>Login</a>
-            </Link>
-          </p>
+  useEffect(() => {
+    router.push('/profiles');
+  }, []);
 
-          <div className={styles.grid}>
-            <a href="https://nextjs.org/docs" className={styles.card}>
-              <h3>Documentation &rarr;</h3>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </a>
-
-            <a href="https://nextjs.org/learn" className={styles.card}>
-              <h3>Learn &rarr;</h3>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
-
-            <a
-              href="https://github.com/vercel/next.js/tree/master/examples"
-              className={styles.card}
-            >
-              <h3>Examples &rarr;</h3>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-              href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-            >
-              <h3>Deploy &rarr;</h3>
-              <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-            </a>
-          </div>
-        </main>
-      </div>
-    </Layout>
-  );
+  return null;
 }
