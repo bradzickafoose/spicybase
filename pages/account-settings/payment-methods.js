03f36cbd8ff42a50047d6210280b5f18678ce9fc
Brad <brad@viziworx.com>
2020-09-13 23:45:18 -0400

    🐛 FIX: updates page heading component name

--- pages/account-settings/payment-methods.js
+++ pages/account-settings/payment-methods.js
@@ -1,14 +1,14 @@
-import { Breadcrumbs, ContentPlaceholder, PageTitle, Layout } from 'components';
+import { Breadcrumbs, Container, Layout, PageHeading, PlaceholderSection } from 'components';
 
 export default function PaymentsPage() {
   return (
     <Layout title="Payments &amp; payouts" pageTitle={false}>
-      <PageTitle title="Payments &amp; payouts">
+      <PageHeading title="Payments &amp; payouts">
         <Breadcrumbs title="Payments &amp; payouts" />
-      </PageTitle>
-      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <ContentPlaceholder />
-      </div>
+      </PageHeading>
+      <Container>
+        <PlaceholderSection />
+      </Container>
     </Layout>
   );
 }
--- pages/account-settings/personal-info.js
+++ pages/account-settings/personal-info.js
@@ -1,14 +1,14 @@
-import { Breadcrumbs, ContentPlaceholder, PageTitle, Layout } from 'components';
+import { Breadcrumbs, Container, Layout, PageHeading, PlaceholderSection } from 'components';
 
 export default function PersonalInfoPage() {
   return (
     <Layout title="Personal info" pageTitle={false}>
-      <PageTitle title="Personal info">
+      <PageHeading title="Personal info">
         <Breadcrumbs title="Personal info" />
-      </PageTitle>
-      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <ContentPlaceholder />
-      </div>
+      </PageHeading>
+      <Container>
+        <PlaceholderSection />
+      </Container>
     </Layout>
   );
 }
