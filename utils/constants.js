6f001df2fee51a0b31fe1e77038026135e44ac9b
Brad <brad@viziworx.com>
2020-09-14 00:00:50 -0400

    👌 IMPROVE: adds iso languages

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,16 @@
 export const APP_NAME = 'Spicybase';
 export const BASE_URL = 'https://spicybase.vercel.app/';
+
+export const ISO_LANGUAGES = {
+  AR: 'Arabic',
+  ZH: 'Chinese',
+  EN: 'English',
+  FR: 'French',
+  DE: 'German',
+  IT: 'Italian',
+  JA: 'Japanese',
+  KO: 'Korean',
+  PT: 'Portuguese',
+  ES: 'Spanish',
+  SGN: 'Sign Language',
+};
