2485e1ec245815fe4a8ddaa23c84f890babe89b1
Brad <brad@viziworx.com>
2020-09-19 15:57:21 -0400

    📦 NEW: adds personal information form

--- components/forms/index.js
+++ components/forms/index.js
@@ -1,4 +1,5 @@
 export { CreateJobForm } from './create-job-form';
 export { LoginForm } from './login-form';
+export { PersonalInfoForm } from './personal-info-form';
 export { ProfileForm } from './profile-form';
 export { RegistrationForm } from './registration-form';
--- pages/account-settings/personal-info.js
+++ pages/account-settings/personal-info.js
@@ -1,13 +1,17 @@
-import { Breadcrumbs, Container, Layout, PageHeading, PlaceholderSection } from 'components';
+import { useContext } from 'react';
+import { Breadcrumbs, Container, Layout, PageHeading, PersonalInfoForm } from 'components';
+import { UserContext } from 'providers/UserProvider';
 
 export default function PersonalInfoPage() {
+  const { user } = useContext(UserContext);
+
   return (
-    <Layout title="Personal info" pageTitle={false}>
+    <Layout title="Personal info" pageTitle={false} footer={false}>
       <PageHeading title="Personal info">
         <Breadcrumbs title="Personal info" />
       </PageHeading>
       <Container>
-        <PlaceholderSection />
+        <PersonalInfoForm user={user} />
       </Container>
     </Layout>
   );
