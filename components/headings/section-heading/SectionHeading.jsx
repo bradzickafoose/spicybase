aa92cf3db2043742f5ed71d1ba08ae13bbfdc5b4
Brad <brad@viziworx.com>
2020-09-19 15:59:11 -0400

    👌 IMPROVE: updates styling

--- components/headings/section-heading/SectionHeading.jsx
+++ components/headings/section-heading/SectionHeading.jsx
@@ -1,7 +1,7 @@
 export default function SectionHeading({ title, children }) {
   return (
-    <div className="pb-5 space-y-3 border-b border-gray-200 sm:flex sm:items-center sm:justify-between sm:space-x-4 sm:space-y-0">
-      <h3 className="text-xl font-medium leading-6 text-gray-900">{title}</h3>
+    <div className="pb-5 space-y-3 sm:flex sm:items-center sm:justify-between sm:space-x-4 sm:space-y-0">
+      <h3 className="text-3xl font-semibold leading-6 text-gray-900">{title}</h3>
       <div>{children}</div>
     </div>
   );
