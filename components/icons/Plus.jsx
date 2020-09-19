91a701a043cdd2fb6c656572f36b51d564fe49ee
Brad <brad@viziworx.com>
2020-09-19 15:54:06 -0400

    📦 NEW: adds icons

--- components/icons/Plus.jsx
+++ components/icons/Plus.jsx
@@ -1,6 +1,6 @@
 export default function PlusIcon(props) {
   return (
-    <svg viewBox="0 0 20 20" fill="currentColor" {...props}>
+    <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
         fillRule="evenodd"
         d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
--- components/icons/index.js
+++ components/icons/index.js
@@ -1,1 +1,4 @@
-export { PlusIcon } from './Plus';
+export { default as ChatIcon } from './Chat';
+export { default as HomeIcon } from './Home';
+export { default as PlusIcon } from './Plus';
+export { default as StarIcon } from './Star';
