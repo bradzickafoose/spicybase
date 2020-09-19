38788e6f123d223b4e6085db085643f5b5547fe7
Brad <brad@viziworx.com>
2020-09-19 16:00:50 -0400

    👌 IMPROVE: updates form inputs

--- components/forms/profile-form/ProfileForm.jsx
+++ components/forms/profile-form/ProfileForm.jsx
@@ -2,20 +2,21 @@ import { useContext, useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import { axiosWithAuth } from 'utils/axiosWithAuth';
 import { UserContext } from 'providers/UserProvider';
+import Avatar from 'components/common/avatar/Avatar';
 
 export default function ProfileForm({ user, setEditAccount }) {
   const { register, handleSubmit, errors } = useForm({ defaultValues: { ...user } });
   const { setUser } = useContext(UserContext);
   const profileImageRef = useRef();
 
-  const convertProfileImageToBase64 = (file) => {
+  const convertProfileImageToBase64 = file => {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
-      reader.readAsDataURL(file)
+      reader.readAsDataURL(file);
       reader.onload = () => resolve(reader.result);
-      reader.onerror = (error) => reject(error);
-    })
-  }
+      reader.onerror = error => reject(error);
+    });
+  };
 
   const onSubmit = async user => {
     const files = profileImageRef.current.files;
@@ -37,12 +38,6 @@ export default function ProfileForm({ user, setEditAccount }) {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mt-6">
         <div>
-          <div>
-            <h3 className="text-lg font-medium leading-6 text-gray-900">Profile</h3>
-            <p className="mt-1 text-sm leading-5 text-gray-500">
-              This information will be displayed publicly so be careful what you share.
-            </p>
-          </div>
           <div className="grid grid-cols-1 row-gap-6 col-gap-4 mt-6 sm:grid-cols-6">
             <div className="sm:col-span-4">
               <label
@@ -51,15 +46,15 @@ export default function ProfileForm({ user, setEditAccount }) {
               >
                 Username
               </label>
-              <div className="flex mt-1 rounded-md shadow-sm">
-                <span className="inline-flex items-center px-3 text-gray-500 bg-transparent border border-r-0 border-gray-300 rounded-l-md sm:text-sm">
-                  utiliteams.com/
-                </span>
+              <div className="relative mt-1 rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <span className="text-gray-400 sm:text-sm sm:leading-5">utiliteams.com/</span>
+                </div>
                 <input
                   name="username"
                   id="username"
                   ref={register}
-                  className="flex-1 block w-full min-w-0 transition duration-150 ease-in-out rounded-none form-input rounded-r-md sm:text-sm sm:leading-5"
+                  className="block w-full pl-16 form-input sm:pl-28 sm:text-sm sm:leading-5"
                 />
               </div>
             </div>
@@ -80,22 +75,38 @@ export default function ProfileForm({ user, setEditAccount }) {
               <p className="mt-2 text-sm text-gray-500">Write a few sentences about yourself.</p>
             </div>
 
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="current_location"
+                className="block text-sm font-medium leading-5 text-gray-700"
+              >
+                Current Location
+              </label>
+              <div className="mt-1 rounded-md shadow-sm">
+                <input
+                  name="currentLocation"
+                  id="current_location"
+                  type="text"
+                  ref={register}
+                  className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
+                />
+              </div>
+            </div>
+
             <div className="sm:col-span-6">
               <label htmlFor="photo" className="block text-sm font-medium leading-5 text-gray-700">
                 Photo
               </label>
               <div className="flex items-center mt-2">
-                <span className="w-12 h-12 overflow-hidden bg-gray-100 rounded-full">
-                  <svg
-                    className="w-full h-full text-gray-300"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                  </svg>
-                </span>
-                <span className="ml-5 rounded-md shadow-sm">
-                  <input name="profileImage" type="file" ref={profileImageRef} className="px-3 py-2 text-sm font-medium leading-4 text-gray-700 transition duration-150 ease-in-out bg-white border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"/>
+                <Avatar src={user.profileImage} size="small" />
+                <span className="ml-5">
+                  <input
+                    name="profileImage"
+                    accept="image/jpeg, image/png"
+                    type="file"
+                    ref={profileImageRef}
+                    className="px-3 py-2 text-sm font-medium leading-4 text-gray-700 hover:text-gray-500 active:text-gray-800"
+                  />
                 </span>
               </div>
             </div>
@@ -103,169 +114,34 @@ export default function ProfileForm({ user, setEditAccount }) {
         </div>
         <div className="pt-8 mt-8 border-t border-gray-200">
           <div>
-            <h3 className="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
+            <h3 className="text-lg font-medium leading-6 text-gray-900">Job preferences</h3>
             <p className="mt-1 text-sm leading-5 text-gray-500">
-              Use a permanent address where you can receive mail.
+              We use this information to match you with the right jobs and help you save time on
+              your job applications. It'll only be shown to companies if you've expressed interest
+              in being contacted. Your salary is private and will never be shown to companies.
             </p>
           </div>
-          <div className="grid grid-cols-1 row-gap-6 col-gap-4 mt-6 sm:grid-cols-6">
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="first_name"
-                className="block text-sm font-medium leading-5 text-gray-700"
-              >
-                First name
-              </label>
-              <div className="mt-1 rounded-md shadow-sm">
-                <input
-                  name="firstName"
-                  id="first_name"
-                  ref={register}
-                  className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
-                />
-              </div>
-            </div>
 
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="last_name"
-                className="block text-sm font-medium leading-5 text-gray-700"
-              >
-                Last name
-              </label>
-              <div className="mt-1 rounded-md shadow-sm">
-                <input
-                  name="lastName"
-                  id="last_name"
-                  ref={register}
-                  className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-4">
-              <label htmlFor="email" className="block text-sm font-medium leading-5 text-gray-700">
-                Email address
-              </label>
-              <div className="mt-1 rounded-md shadow-sm">
-                <input
-                  name="email"
-                  id="email"
-                  type="email"
-                  ref={register}
-                  className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="street_address"
-                className="block text-sm font-medium leading-5 text-gray-700"
-              >
-                Street address
-              </label>
-              <div className="mt-1 rounded-md shadow-sm">
-                <input
-                  name="addressStreet"
-                  id="street_address"
-                  placeholder="Ex: 123 Main Street"
-                  ref={register}
-                  className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-3">
-              <label
-                htmlFor="optional_address"
-                className="block text-sm font-medium leading-5 text-gray-700"
-              >
-                Apt, suite (optional)
-              </label>
-              <div className="mt-1 rounded-md shadow-sm">
-                <input
-                  name="addressOptional"
-                  id="optional_address"
-                  placeholder="Ex: Apt #1015"
-                  ref={register}
-                  className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-2">
-              <label htmlFor="city" className="block text-sm font-medium leading-5 text-gray-700">
-                City
-              </label>
-              <div className="mt-1 rounded-md shadow-sm">
-                <input
-                  name="city"
-                  id="city"
-                  placeholder="Ex: Mayberry"
-                  ref={register}
-                  className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-2">
-              <label htmlFor="state" className="block text-sm font-medium leading-5 text-gray-700">
-                State
-              </label>
-              <div className="mt-1 rounded-md shadow-sm">
-                <input
-                  name="state"
-                  id="state"
-                  placeholder="Ex: NC"
-                  ref={register}
-                  className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-2">
-              <label htmlFor="zip" className="block text-sm font-medium leading-5 text-gray-700">
-                ZIP code
-              </label>
-              <div className="mt-1 rounded-md shadow-sm">
-                <input
-                  name="zip"
-                  id="zip"
-                  placeholder="Ex: 27030"
-                  ref={register}
-                  className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="pt-8 mt-8 border-t border-gray-200">
-          <div>
-            <h3 className="text-lg font-medium leading-6 text-gray-900">Notifications</h3>
-            <p className="mt-1 text-sm leading-5 text-gray-500">
-              We'll always let you know about important changes, but you pick what else you want to
-              hear about.
-            </p>
-          </div>
           <div className="mt-6">
             <fieldset>
-              <legend className="text-base font-medium text-gray-900">By Email</legend>
+              <legend className="text-base font-medium text-gray-900">Job search status</legend>
               <div className="mt-4">
                 <div className="relative flex items-start">
                   <div className="flex items-center h-5">
                     <input
-                      id="comments"
-                      type="checkbox"
-                      className="w-4 h-4 text-green-600 transition duration-150 ease-in-out form-checkbox"
+                      name="availability"
+                      id="status_active"
+                      type="radio"
+                      className="w-4 h-4 text-green-600 transition duration-150 ease-in-out form-radio"
                     />
                   </div>
                   <div className="ml-3 text-sm leading-5">
-                    <label htmlFor="comments" className="font-medium text-gray-700">
-                      Comments
+                    <label htmlFor="status_active" className="font-medium text-gray-700">
+                      Actively looking right now
                     </label>
                     <p className="text-gray-500">
-                      Get notified when someones posts a comment on a posting.
+                      Maximum matches. We'll get you in front of employers ASAP and send you any new
+                      jobs that match your interests.
                     </p>
                   </div>
                 </div>
@@ -273,17 +149,19 @@ export default function ProfileForm({ user, setEditAccount }) {
                   <div className="relative flex items-start">
                     <div className="flex items-center h-5">
                       <input
-                        id="candidates"
-                        type="checkbox"
-                        className="w-4 h-4 text-green-600 transition duration-150 ease-in-out form-checkbox"
+                        name="availability"
+                        id="status_open"
+                        type="radio"
+                        className="w-4 h-4 text-green-600 transition duration-150 ease-in-out form-radio"
                       />
                     </div>
                     <div className="ml-3 text-sm leading-5">
-                      <label htmlFor="candidates" className="font-medium text-gray-700">
-                        Candidates
+                      <label htmlFor="status_open" className="font-medium text-gray-700">
+                        Open, but not actively looking
                       </label>
                       <p className="text-gray-500">
-                        Get notified when a candidate applies for a job.
+                        Fewer matches. Employers can find you and we'll be selective with the
+                        matches we send.
                       </p>
                     </div>
                   </div>
@@ -292,67 +170,21 @@ export default function ProfileForm({ user, setEditAccount }) {
                   <div className="relative flex items-start">
                     <div className="flex items-center h-5">
                       <input
-                        id="offers"
-                        type="checkbox"
-                        className="w-4 h-4 text-green-600 transition duration-150 ease-in-out form-checkbox"
+                        name="availability"
+                        id="status_inactive"
+                        type="radio"
+                        className="w-4 h-4 text-green-600 transition duration-150 ease-in-out form-radio"
                       />
                     </div>
                     <div className="ml-3 text-sm leading-5">
-                      <label htmlFor="offers" className="font-medium text-gray-700">
-                        Offers
+                      <label htmlFor="status_inactive" className="font-medium text-gray-700">
+                        Not interested in jobs
                       </label>
                       <p className="text-gray-500">
-                        Get notified when a candidate accepts or rejects an offer.
+                        No matches. Employers won't find you and we won't send you any matches.
                       </p>
                     </div>
                   </div>
-                </div>
-              </div>
-            </fieldset>
-            <fieldset className="mt-6">
-              <legend className="text-base font-medium text-gray-900">Push Notifications</legend>
-              <p className="text-sm leading-5 text-gray-500">
-                These are delivered via SMS to your mobile phone.
-              </p>
-              <div className="mt-4">
-                <div className="flex items-center">
-                  <input
-                    id="push_everything"
-                    name="push_notifications"
-                    type="radio"
-                    className="w-4 h-4 text-green-600 transition duration-150 ease-in-out form-radio"
-                  />
-                  <label htmlFor="push_everything" className="ml-3">
-                    <span className="block text-sm font-medium leading-5 text-gray-700">
-                      Everything
-                    </span>
-                  </label>
-                </div>
-                <div className="flex items-center mt-4">
-                  <input
-                    id="push_email"
-                    name="push_notifications"
-                    type="radio"
-                    className="w-4 h-4 text-green-600 transition duration-150 ease-in-out form-radio"
-                  />
-                  <label htmlFor="push_email" className="ml-3">
-                    <span className="block text-sm font-medium leading-5 text-gray-700">
-                      Same as email
-                    </span>
-                  </label>
-                </div>
-                <div className="flex items-center mt-4">
-                  <input
-                    id="push_nothing"
-                    name="push_notifications"
-                    type="radio"
-                    className="w-4 h-4 text-green-600 transition duration-150 ease-in-out form-radio"
-                  />
-                  <label htmlFor="push_nothing" className="ml-3">
-                    <span className="block text-sm font-medium leading-5 text-gray-700">
-                      No push notifications
-                    </span>
-                  </label>
                 </div>
               </div>
             </fieldset>
@@ -364,7 +196,7 @@ export default function ProfileForm({ user, setEditAccount }) {
           <span className="inline-flex rounded-md shadow-sm">
             <button
               type="button"
-              onClick={()=> setEditAccount(false)}
+              onClick={() => setEditAccount(false)}
               className="px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out border border-gray-300 rounded-full hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
             >
               Cancel
